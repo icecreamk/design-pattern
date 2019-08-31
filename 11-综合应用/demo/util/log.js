e895f98c95e6e439df04e4d89d5c6abacea124f4
kkxxll <550893685@qq.com>
2019-08-31 11:21:03 +0800

    综合demo

---
+++ 11-综合应用/demo/util/log.js
@@ -0,0 +1,16 @@
+// 装饰器模式
+export function log(type) {
+    return function (target, name, descriptor) {
+        var oldValue = descriptor.value;
+    
+        descriptor.value = function() {
+            //  此处统一上报日志
+            console.log(`日志上报 ${type}`);
+    
+            // 执行原有方法
+            return oldValue.apply(this, arguments);
+        };
+    
+        return descriptor;
+    }
+}
